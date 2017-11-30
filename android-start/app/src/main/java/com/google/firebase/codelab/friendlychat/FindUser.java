98ed6135ea41311a205e49e2b065a8a17d655d49
Sarim Ahmed <sarim987@gmail.com>
2017-11-30 00:49:50 -0500

    Added Find User Activity

---
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/FindUser.java
@@ -0,0 +1,16 @@
+package com.google.firebase.codelab.friendlychat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_user);
+
+
+    }
+
+}
