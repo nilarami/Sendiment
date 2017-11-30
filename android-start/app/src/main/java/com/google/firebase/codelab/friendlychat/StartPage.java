a4e0fd645f45c23b4015c12e02910ec7745fb9e8
Sarim Ahmed <sarim987@gmail.com>
2017-11-30 00:15:27 -0500

    Created SignUp Activity

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/StartPage.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/StartPage.java
@@ -12,11 +12,20 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
-        Button btn_login = (Button) findViewById(R.id.login_btn);
+        Button btn_login = findViewById(R.id.login_btn);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
                 startActivity(i);
             }
         });
